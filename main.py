de205ef8fd52e910412e255d5e5cdc1a6b90dba6
Afonso Alcantara <alcantara.afonso@gmail.com>
2024-08-16 19:06:41 -0300

    Vector DB - Pinecone

--- main.py
+++ main.py
@@ -9,8 +9,13 @@ from langchain_pinecone import PineconeVectorStore
 from langchain import hub
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.retrieval import create_retrieval_chain
+from langchain_core.runnables import RunnablePassthrough
 
 load_dotenv()
+
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
 
 if __name__ == "__main__":
     print("Interesting...")
@@ -20,9 +25,9 @@ if __name__ == "__main__":
 
     query = "what is Pinecone in machine learning?" 
     chain = PromptTemplate.from_template(template=query) | llm
-    result = chain.invoke(input={})  
+    # result = chain.invoke(input={})  
 
-    print(result.content)
+    # print(result.content)
 
     vector_store = PineconeVectorStore(
         index_name=os.environ["INDEX_NAME"], 
@@ -38,4 +43,27 @@ if __name__ == "__main__":
     )
 
     result = retrival_chain.invoke(input={"input": query})
-    print(result.page_content)
+    print(result)
+
+    template = """ 
+        Use the following pieces of context to answer the question at the end.
+        If you don't know the answer, just say that you don't know, don't try to make up an answer.
+        Use three sentences maximm and keep the answer as concise as possible.
+        Always say "thanks for asking!" at the end of the answer.
+
+        {context}
+
+        Quastion: {question}
+
+        Helpful Answer: """
+    
+    custom_rag_prompt = PromptTemplate.from_template(template=template)
+    rag_chain = (
+        { "context": vector_store.as_retriever() | format_docs, "question":  RunnablePassthrough() }
+        | custom_rag_prompt
+        | llm
+    )
+
+    res = rag_chain.invoke(query)
+
+    print(res)
